2c26f65d4bec6e3d79c51974f0cc3f8943ab6d59
Sh0bhit <sguptamds94@gmail.com>
2023-08-15 19:00:58 +0530

    Expertise animations

--- src/components/Expertise/ExpertiseLeft.tsx
+++ src/components/Expertise/ExpertiseLeft.tsx
@@ -1,13 +1,36 @@
+import { MutableRefObject, useEffect, useRef } from "react";
 import Button, { buttonColors } from "../UI/Button/Button";
 import Label, { labelColors } from "../UI/Label/Label";
 import SlideText, { SlideTextColors } from "../UI/SlideText/SlideText";
 import Arrow from "../icons/Arrow";
 import Eye from "../icons/Eye";
 import Fingers from "../icons/Fingers";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function ExpertiseLeft() {
+  const paraRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      paraRef.current,
+      { translateY: 150, opacity: 0 },
+      {
+        translateY: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: paraRef.current,
+          scrub: 2,
+          start: "10% bottom",
+          end: "30% bottom",
+        },
+      }
+    );
+  }, []);
+
   return (
-    <div className="flex flex-col gap-5 md:w-[40%]">
+    <div className="flex sticky top-[20%] h-full flex-col gap-5 md:w-[40%]">
       <Label title="Expertise" labelBg={labelColors.green} />
       <SlideText
         label="HOW CAN I HELP"
@@ -16,7 +39,7 @@ export default function ExpertiseLeft() {
         colorTwo={SlideTextColors.gray}
         style="text-[40px] font-medium"
       />
-      <p className="font-general font-medium text-[17px]">
+      <p ref={paraRef} className="font-general font-medium text-[17px]">
         As a freelance Full Stack developer, I specialize in providing custom
         web design and development services to startups and established
         businesses alike. My goal is to assist businesses in building a robust
--- src/components/Expertise/ExpertiseRight.tsx
+++ src/components/Expertise/ExpertiseRight.tsx
@@ -5,55 +5,58 @@ import Label, { labelColors } from "../UI/Label/Label";
 export default function ExpertiseRight() {
   return (
     <div className="flex flex-col md:w-[60%]">
-      {expertiseCards.map((card) => (
-        <div
-          key={card.title}
-          className="sm:w-[80%] mx-auto flex flex-col bg-greyBg gap-10 mt-10 ss:py-10 max-ss:pb-7 max-ss:pt-3 px-5 rounded-2xl text-whiteShade"
-        >
-          <div className="flex ss:flex-row flex-col ss:gap-7">
-            <Image
-              src={card.image}
-              alt={card.title}
-              width={100}
-              height={100}
-              className="p-5"
-            />
-            <div className="flex flex-col gap-2">
-              <h1 className="font-satoshi text-[25px] font-medium ">
-                {card.title}
-              </h1>
-              <div className="flex gap-3 flex-wrap">
-                {card.tags.map((tag, idx) => {
-                  const arr: labelColors[] = [
-                    labelColors.gray,
-                    labelColors.green,
-                    labelColors.transparant,
-                  ];
+      {expertiseCards.map((card, idx) => {
+        return (
+          <div
+            key={card.title}
+            className={`md:w-[80%] sticky border mx-auto h-full flex flex-col bg-greyBg gap-10 mt-10 ss:py-10 max-ss:pb-7 max-ss:pt-3 px-5 rounded-2xl text-whiteShade`}
+            style={{ top: String(20 + idx * 2) + "%" }}
+          >
+            <div className="flex ss:flex-row flex-col ss:gap-7">
+              <Image
+                src={card.image}
+                alt={card.title}
+                width={100}
+                height={100}
+                className="p-5"
+              />
+              <div className="flex flex-col gap-2">
+                <h1 className="font-satoshi text-[25px] font-medium">
+                  {card.title}
+                </h1>
+                <div className="flex gap-3 flex-wrap">
+                  {card.tags.map((tag, idx) => {
+                    const arr: labelColors[] = [
+                      labelColors.gray,
+                      labelColors.green,
+                      labelColors.transparant,
+                    ];
 
-                  const selectedColor: labelColors =
-                    idx === Math.floor(Math.random() * arr.length)
-                      ? arr[idx]
-                      : arr[idx - 1] || arr[idx + 1];
+                    const selectedColor: labelColors =
+                      idx === Math.floor(Math.random() * arr.length)
+                        ? arr[idx]
+                        : arr[idx - 1] || arr[idx + 1];
 
-                  return (
-                    <Label
-                      title={tag}
-                      key={tag}
-                      labelBg={selectedColor}
-                      styles={`${
-                        selectedColor === labelColors.transparant
-                          ? "border border-whiteShade"
-                          : ""
-                      } text-whiteShade`}
-                    />
-                  );
-                })}
+                    return (
+                      <Label
+                        title={tag}
+                        key={tag}
+                        labelBg={selectedColor}
+                        styles={`${
+                          selectedColor === labelColors.transparant
+                            ? "border border-whiteShade"
+                            : ""
+                        } text-whiteShade`}
+                      />
+                    );
+                  })}
+                </div>
+                <p className="font-general mt-10">{card.desc}</p>
               </div>
-              <p className="font-general mt-10">{card.desc}</p>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
