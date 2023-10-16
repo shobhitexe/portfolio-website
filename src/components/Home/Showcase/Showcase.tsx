8114559635ebda7f42436628d423125cc4279422
Sh0bhit <sguptamds94@gmail.com>
2023-10-16 16:13:06 +0530

    Showcase Section animation

--- src/components/Home/Showcase/Showcase.tsx
+++ src/components/Home/Showcase/Showcase.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { useEffect, MutableRefObject, useState } from "react";
+import { useEffect, useState } from "react";
 import { showCaseImages, showCaseImagesType } from "./showCaseConstants";
+import { motion } from "framer-motion";
 
 export default function Showcase() {
   const [gridImages, setGridImages] = useState<showCaseImagesType[][]>([]);
@@ -24,13 +25,25 @@ export default function Showcase() {
   return (
     <div className="overflow-hidden bg-whiteShade bg-opacity-20 mt-40 sm:h-screen h-[50vh]">
       <div className="flex sm:gap-5 gap-1">
-        {[0, 1, 2].map((num) => (
-          <div
+        {[0, 1, 2].map((num, indx) => (
+          <motion.div
             key={num}
             className={`flex flex-col sm:gap-4 ss:gap-16 xs:gap-16 gap-10`}
           >
             {gridImages[num]?.map((img, idx) => (
-              <div
+              <motion.div
+                initial={{
+                  translateY: `${indx === 1 ? "-50%" : "50%"}`,
+                  opacity: 0,
+                }}
+                whileInView={{ translateY: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.1,
+                  duration: 0.5,
+                  stiffness: 50,
+                  damping: 10,
+                  type: "spring",
+                }}
                 key={Math.random() + num + idx}
                 className={`shadow-xl  ${
                   num === 2
@@ -47,9 +60,9 @@ export default function Showcase() {
                   height={500}
                   className={`rounded-md w-auto h-auto`}
                 />
-              </div>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
