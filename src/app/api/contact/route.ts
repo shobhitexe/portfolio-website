c96c0ba592470f8034229599f175332e137ad3ab
shobhitexe <sguptamds94@gmail.com>
2024-02-21 11:10:23 +0530

    Contact route body fix

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,22 +5,22 @@ export async function POST(request: Request) {
   try {
     const req = await request.json();
 
-    if (!req.body) {
-      return NextResponse.error();
+    if (!req) {
+      return NextResponse.json({ error: "No body provided" }, { status: 400 });
     }
 
     await transporter.sendMail({
       ...mailOptions,
       subject: "New query from portfolio website",
       text: "",
-      html: `<h1>Name - ${req.body.name}</h1>
-      <p>E-mail - ${req.body.email}</P>
-      <p>Message - ${req.body.message}</p>
-      <p>How did he find me - ${req.body.find || ""}</p>`,
+      html: `<h1>Name - ${req.name}</h1>
+      <p>E-mail - ${req.email}</P>
+      <p>Message - ${req.message}</p>
+      <p>How did he find me - ${req.find || ""}</p>`,
     });
 
     return NextResponse.json({ success: "Form submitted" }, { status: 200 });
   } catch (error) {
-    return NextResponse.error();
+    return NextResponse.json({ error }, { status: 400 });
   }
 }
